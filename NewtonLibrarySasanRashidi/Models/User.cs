c8b68b381d2493035e05b292a8fc8e04665237fc
sasanrashidi <sassi_88@hotmail.com>
2023-12-10 15:44:19 +0100

    Skapat en User klass

---
+++ NewtonLibrarySasanRashidi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewtonLibrarySasanRashidi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+
+        public UserCard? userCard { get; set; }
+
+    }
+}
