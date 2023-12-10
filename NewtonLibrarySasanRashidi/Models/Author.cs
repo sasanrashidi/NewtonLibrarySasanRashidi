cff512191af22001abca6a4eb15bab68be2d2bd8
sasanrashidi <sassi_88@hotmail.com>
2023-12-10 15:42:22 +0100

    Skapat en Author klass

---
+++ NewtonLibrarySasanRashidi/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewtonLibrarySasanRashidi.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+
+        public string Name { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+
+
+    }
+}
+
