6d3168da7870edca3e285f18259f513177c9b80e
sasanrashidi <sassi_88@hotmail.com>
2023-12-10 15:45:09 +0100

    Skapat en UserCard klass

---
+++ NewtonLibrarySasanRashidi/Models/UserCard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NewtonLibrarySasanRashidi.Models
+{
+    public class UserCard
+    {
+        public int Id { get; set; }
+
+        public int Pin { get; set; } = new Random().Next(1000, 9999);
+
+        public ICollection<Book>? Books { get; set; }
+    }
+}
