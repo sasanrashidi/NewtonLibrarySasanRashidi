1134c498bc9d836482560b85dc3c087e8a9def89
sasanrashidi <sassi_88@hotmail.com>
2023-12-10 23:40:43 +0100

    Färdit

--- NewtonLibrarySasanRashidi/Migrations/20231210223412_initial.Designer.cs
+++ NewtonLibrarySasanRashidi/Migrations/20231210223412_initial.Designer.cs
@@ -12,7 +12,7 @@ using NewtonLibrarySasanRashidi.Data;
 namespace NewtonLibrarySasanRashidi.Migrations
 {
     [DbContext(typeof(Context))]
-    [Migration("20231210222237_initial")]
+    [Migration("20231210223412_initial")]
     partial class initial
     {
         /// <inheritdoc />
