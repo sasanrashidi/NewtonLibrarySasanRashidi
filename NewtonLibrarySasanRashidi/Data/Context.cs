ed74599f96491ff2c7ab750f7a753f9a1c9fdecb
sasanrashidi <sassi_88@hotmail.com>
2023-12-10 15:38:06 +0100

    Skapat en klass som heter Context

---
+++ NewtonLibrarySasanRashidi/Data/Context.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewtonLibrarySasanRashidi.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserCard> UserCards { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .Property(bk => bk.BookIsLoaned)
+                .HasColumnName("Loaned")
+                .HasColumnType("bit");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"
+                    Server=localhost;
+                    Database=NewtonLibrarySasanRashidi;
+                    Trust Server Certificate = Yes;
+                    User Id=NewtonLibrarySasanRashidi;
+                    Password=NewtonLibrarySasanRashidi");
+        }
+    }
+}
+
+
+
